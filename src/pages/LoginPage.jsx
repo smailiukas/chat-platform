b825044db75f885e32d5c8a5fcf91da107853a05
smailiukas <ronis.vezlys@gmail.com>
2022-09-14 16:50:17 +0300

    modified:   .env modified:   .gitignore deleted:    README.md modified:   package-lock.json modified:   package.json modified:   src/pages/LoginPage.jsx modified:   src/pages/RegisterPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,11 +1,69 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import styled from "styled-components";
 import { useNavigate, Link } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { loginRoute } from "../utils/APIRoutes";
 
 function LoginPage() {
+
+  const navigate = useNavigate();
+  const [values, setValues] = useState({ username: "", password: "" });
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
+  useEffect(() => {
+    if (localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
+      navigate("/");
+    }
+  }, []);
+
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+
+  const validateForm = () => {
+    const { username, password } = values;
+    if (username === "") {
+      toast.error("Email and Password is required.", toastOptions);
+      return false;
+    } else if (password === "") {
+      toast.error("Email and Password is required.", toastOptions);
+      return false;
+    }
+    return true;
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (validateForm()) {
+      const { username, password } = values;
+      const { data } = await axios.post(loginRoute, {
+        username,
+        password,
+      });
+      if (data.status === false) {
+        toast.error(data.msg, toastOptions);
+      }
+      if (data.status === true) {
+        localStorage.setItem(
+          process.env.REACT_APP_LOCALHOST_KEY,
+          JSON.stringify(data.user)
+        );
+
+        navigate("/");
+      }
+  }
+};
   return (
     <>
       <FormContainer>
-        <form action="" >
+        <form action="" onSubmit={(event) => handleSubmit(event)}>
           <div className="brand">
             <h1>CHAT APP</h1>
           </div>
@@ -13,12 +71,14 @@ function LoginPage() {
             type="text"
             placeholder="Username"
             name="username"
+            onChange={(e) => handleChange(e)}
             min="3"
           />
           <input
             type="password"
             placeholder="Password"
             name="password"
+            onChange={(e) => handleChange(e)}
           />
           <button type="submit">Log In</button>
           <span>
@@ -26,6 +86,7 @@ function LoginPage() {
           </span>
         </form>
       </FormContainer>
+      <ToastContainer />
       </>
   );
 }
